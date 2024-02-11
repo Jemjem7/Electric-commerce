c47020c1266730e6b12b17e77de240bfa18f036f
Jemjem7 <benzjeramtinga777@gmail.com>
2024-02-11 22:18:49 +0800

    added all sapatos prod

---
+++ products namo/our.js
@@ -0,0 +1,21 @@
+function navigateTo(page) {
+    // Hide all content sections
+    const contentSections = document.querySelectorAll('main');
+    contentSections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    // Show the selected content section
+    const selectedSection = document.getElementById(page);
+    selectedSection.style.display = 'block';
+}
+
+// Initial navigation to the home page
+navigateTo('home');
+
+function page1() {
+    window.location.href = 'index.html';
+  }
+  function page2() {
+    window.location.href = 'reg.html';
+  }
